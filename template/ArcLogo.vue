ccc9a737f72adb8b30d4c5c708ecc2ff787570b4
BarthyB <barthy.bonhomme@gmail.com>
2019-10-19 00:12:50 +0200

    initial commit

---
+++ template/ArcLogo.vue
@@ -0,0 +1,17 @@
+<template functional
+          v-once
+          src="./arc-logo.html"
+/>
+
+<script lang="js">
+  export default {
+    name: 'arc-logo'
+  }
+</script>
+
+<style lang="scss"
+       scoped
+>
+    @import '../src/logo-style';
+    @import '../src/logo-animation';
+</style>
